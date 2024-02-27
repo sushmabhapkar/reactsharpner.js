c16b32fbbe86edd2c513c73ab8b58278b3961268
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-02-27 13:51:28 +0530

    m2 task1(delete expense)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import ExpenseItem from "./component/Expense/ExpenseItem";
 
 const App=()=> {
   const expenses = [
-    {
+    { 
+      id:1,
       title: "Car Insurance and life insurances",
       amount: 4533,
       date: new Date(2021, 6, 28)
     },
     {
+      id:2,
       title: "Shopping Clothes and some footwears",
       amount: 6663,
       date: new Date(2021, 5, 8) // Corrected the date here
     },
     {
+      id:3,
       title: "General Store Material",
       amount: 9333,
       date: new Date(2021, 6, 28)
@@ -23,16 +26,19 @@ const App=()=> {
     <div>
       <h1>......Expense Track Application.........</h1>
       <ExpenseItem 
+        id={expenses[0].id}
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
       />
       <ExpenseItem 
+        id={expenses[1].id}
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
       />
       <ExpenseItem
+        id={expenses[2].id}
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
--- src/component/Expense/ExpenseItem.js
+++ src/component/Expense/ExpenseItem.js
@@ -1,26 +1,36 @@
-import ExpenseDate from "../Expense/ExpenseDate"; 
-import "../Expense/ExpenseItem.css"; 
-import Card from '../UI/Card';  
+import React from 'react';
+import ExpenseDate from "../Expense/ExpenseDate";
+import "../Expense/ExpenseItem.css";
+import Card from '../UI/Card';
 
-const ExpenseItem=(props)=> {
+const ExpenseItem = (props) => {
+
+  const deleteExpenseHandler = () => {
+    // You can access the DOM element and remove it directly
+    const expenseItem = document.getElementById(`expenses-${props.id}`);
+    if (expenseItem) {
+      expenseItem.remove();
+    }
+  };
+
   return (
     <div>
-    <div className="expense_item_date">
+      <div className="expense_item_date" id={`expenses-${props.id}`}>
 
-    <Card className="expense_item_date">
-      <ExpenseDate date={props.date} />
-      <div className="expense_item_description">
-        <h2>{props.title}</h2>
-        
-      <div className="expense_item_price">
-        <h2>${props.amount}</h2>
-     </div>
+        <Card className="expense_item_date">
+          <ExpenseDate date={props.date} />
+          <div className="expense_item_description">
+            <h2>{props.title}</h2>
+
+            <div className="expense_item_price">
+              <h2>${props.amount}</h2>
+            </div>
+            <button onClick={deleteExpenseHandler}>Delete Expense</button>
+          </div>
+
+        </Card>
       </div>
-      </Card>
     </div>
-  </div>
-    
-
   );
 }
 
