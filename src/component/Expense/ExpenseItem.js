0a10ea494a555815c1057f8340bc0640a01821c9
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-02-27 16:11:11 +0530

    m2 task2(update expense)

--- src/component/Expense/ExpenseItem.js
+++ src/component/Expense/ExpenseItem.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import ExpenseDate from "../Expense/ExpenseDate";
 import "../Expense/ExpenseItem.css";
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
-  const deleteExpenseHandler = () => {
-    // You can access the DOM element and remove it directly
-    const expenseItem = document.getElementById(`expenses-${props.id}`);
-    if (expenseItem) {
-      expenseItem.remove();
-    }
+  // const deleteExpenseHandler = () => {
+  //   // You can access the DOM element and remove it directly
+  //   const expenseItem = document.getElementById(`expenses-${props.id}`);
+  //   if (expenseItem) {
+  //     expenseItem.remove();
+  //   }
+  // };
+ const[title,setTitle]= useState(props.title);
+console.log("expenseitem evaluated by react");
+
+  const updateExpenseHandler=()=>
+  {
+     setTitle("updated!!")
+     console.log(title);
+
   };
 
   return (
@@ -20,12 +29,13 @@ const ExpenseItem = (props) => {
         <Card className="expense_item_date">
           <ExpenseDate date={props.date} />
           <div className="expense_item_description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
 
             <div className="expense_item_price">
               <h2>${props.amount}</h2>
             </div>
-            <button onClick={deleteExpenseHandler}>Delete Expense</button>
+            {/* <button onClick={deleteExpenseHandler}>Delete Expense</button> */}
+            <button onClick={updateExpenseHandler}>Update Expense</button>
           </div>
 
         </Card>
